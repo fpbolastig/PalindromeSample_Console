ac5f6451d18336a5b05ff2ffe8ff434af3a8bfdc
Dexter Bolastig <Dexter Bolastig@LAPTOP-K773G31O>
2024-07-02 16:18:15 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input a string: ");
+        string sinput = Console.ReadLine();
+
+        if (IsPalindrome(sinput))
+        {
+            Console.WriteLine("String input is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("String input is not a palindrome.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        // this will convert the string to lower
+        str = str.ToLower();
+
+        // this will check if the string value reads the same forward or backward
+        int left = 0;
+        int right = str.Length - 1;
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
